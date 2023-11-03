746adcd23d0c034c349a643a3ce6a4c8ae222bd8
Okeoma Nwabueze <okeomanwabueze@trentu.ca>
2023-11-03 15:41:26 -0600

    fixed ffmpeg bug

--- Video-Processing-Service/src/index.ts
+++ Video-Processing-Service/src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.post('/process-video', (req, res) => {
+    //Get path of  the input video file from request body 
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
 
-app.listen(port), () => {
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad request : missing file path");
+    }
+    ffmpeg(inputFilePath)
+        .output(outputFilePath)
+        .on('end', () => {
+            res.status(200).send('video sucessfully uploaded');
+        })
+        .on('error', (err) => {
+            res.status(500).send('Internal server error');
+        })
+        .save(outputFilePath);
+});
+
+
+
+const  port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log(`Video Processing Service listening at http://localhost:${port}`)
-};
+});
